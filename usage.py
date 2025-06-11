7823a33d1ac047bc2da2ce408b84f9488e1769fe
NadaMohamedElBasel <110432081+NadaMohamedElBasel@users.noreply.github.com>
2025-06-12 01:57:38 +0300

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,9 +1,13 @@
 """This module provides an example usage of the package ."""
 
 import py_calculator
-
+print("Running Python usage test...")
+print("3+10=")
 print(py_calculator.add(3, 10))
+print("10-5=")
 print(py_calculator.sub(10, 5))
+print("10*5=")
 print(py_calculator.mul(10, 5))
+print("10/5=")
 print(py_calculator.divide(10, 5))
 # print(dir(py_calculator_pack))
