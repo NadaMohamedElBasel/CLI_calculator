be297104b973e77d3e29f1b9efe062ba8264d836
NadaMohamedElBasel <110432081+NadaMohamedElBasel@users.noreply.github.com>
2025-06-12 02:09:30 +0300

    Update test_py_calculator.py

--- tests/test_py_calculator.py
+++ tests/test_py_calculator.py
@@ -7,6 +7,8 @@ import py_calculator
 # class MyTestCase(unittest.TestCase):
 #     def test_something(self):
 #         self.assertEqual(True, False)  # add assertion here
+
+
 class TestPyCalculator(unittest.TestCase):
     """Unit tests for py_calculator bindings."""
 
