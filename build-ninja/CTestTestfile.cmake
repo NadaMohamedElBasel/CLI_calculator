0608aacca23e3270a830a877f19281d2cc97e2b5
NadaMohamedElBasel <110432081+NadaMohamedElBasel@users.noreply.github.com>
2025-08-06 23:31:14 +0300

    Delete build-ninja directory

--- build-ninja/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/CLionProjects/CLI_calculator
-# Build directory: C:/Users/Admin/CLionProjects/CLI_calculator/build-ninja
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[calculator_test]=] "C:/Users/Admin/CLionProjects/CLI_calculator/build-ninja/test_calculator.exe")
-set_tests_properties([=[calculator_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/CLionProjects/CLI_calculator/CMakeLists.txt;18;add_test;C:/Users/Admin/CLionProjects/CLI_calculator/CMakeLists.txt;0;")
